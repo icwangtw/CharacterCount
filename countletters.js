b4ac258eeae2f73fc9175eb5434e366c234da64c
Stephen <stephenicwang@gmail.com>
2018-04-11 02:01:08 +0000

    moved folders

---
+++ countletters.js
@@ -0,0 +1,8 @@
+function countLetters(input) {
+  var lettersSliced = []
+  for (var i = 0; i < input.length; i++) {
+    lettersSliced.push(input[i]);
+  }
+  return lettersSliced;
+}
+console.log(countLetters(lighthouselabs))
