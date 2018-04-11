5a005ff94bae6a65e416786234decc3ef320d20a
Stephen <stephenicwang@gmail.com>
2018-04-11 18:16:08 +0000

    finished?

--- countletters.js
+++ countletters.js
@@ -1,5 +1,14 @@
 function countLetters(input) {
-  var noSpaces = input.split(" ").join("");
+  var noSpaces = input.split(" ").join("").toLowerCase();
   var lettersCounted = {};
+  for (var i = 0; i < noSpaces.length; i++) {
+    if (lettersCounted[noSpaces[i]] === undefined) {
+      lettersCounted[noSpaces[i]] = 1;
+    }
+    else {
+      lettersCounted[noSpaces[i]] += 1;
+    }
   }
+  return lettersCounted;
 }
+console.log(countLetters("Burrard Station"))
